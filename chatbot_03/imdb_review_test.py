8713ad2e102beb7f9aaccf49d324c1cd7ad10ba6
blinKiss <blinkiss@naver.com>
2023-06-21 23:09:43 +0900

    21

--- chatbot_03/imdb_review_test.py
+++ chatbot_03/imdb_review_test.py
@@ -31,15 +31,42 @@ print(len(test_sentences))
 from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
 vocab_size = 10000
-oov_tok = '<OOV>'
+embedding_dim = 16
 max_length = 120
 trunc_type = 'post' # 0을 어디에 붙일까?
+oov_tok = '<OOV>'
 
 imdb_token = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
 imdb_token.fit_on_texts(train_sentences)
 imdb_word_index = imdb_token.word_index
 
 imdb_sequences = imdb_token.texts_to_sequences(train_sentences)
+# 자연어 처리로 나온 최종 결과
 imdb_paddeds = pad_sequences(imdb_sequences, maxlen=max_length, truncating=trunc_type)
-print(imdb_sequences[0])
-print(imdb_paddeds[0])
+# print(imdb_sequences[0])
+# print(imdb_paddeds[0])
+
+# 테스트 데이터 시퀀스
+test_seqences = imdb_token.texts_to_sequences(test_sentences)
+test_paddeds = pad_sequences(test_seqences, maxlen=max_length, 
+                             truncating=trunc_type)
+
+# 딥러닝 
+import tensorflow as tf
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(6, activation='relu'),
+  tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+# model.summary()
+
+model.compile(loss='binary_crossentropy', optimizer='adam', 
+              metrics=['accuracy'])
+
+num_epochs = 5
+
+model.fit(imdb_paddeds, train_labels, epochs=num_epochs,
+         validation_data=(test_paddeds, test_labels))
